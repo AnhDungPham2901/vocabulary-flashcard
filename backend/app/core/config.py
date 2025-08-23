b6fca55bff7b23c26b379e4d559ec06d92a4fbb2
Anh Dung Pham <dungpham290198@gmail.com>
2025-08-23 17:52:42 +0700

    changed config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,9 +3,9 @@ from typing import Optional
 
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql://username:password@localhost:5432/vocabulary_flashcard"
-    postgres_user: str = "username"
-    postgres_password: str = "password"
+    database_url: str = "postgresql://danielpham:12345@localhost:5432/vocabulary_flashcard"
+    postgres_user: str = "danielpham"
+    postgres_password: str = "12345"
     postgres_db: str = "vocabulary_flashcard"
     postgres_host: str = "localhost"
     postgres_port: int = 5432
