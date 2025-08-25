845485e53cede1dca5b95a885d9d69e55271a2f5
Anh Dung Pham <dungpham290198@gmail.com>
2025-08-25 11:53:27 +0700

    add design more views

---
+++ frontend-ios/frontend-ios/CardDetail.swift
@@ -0,0 +1,18 @@
+//
+//  CardDetail.swift
+//  frontend-ios
+//
+//  Created by PHAM ANH DUNG on 25/8/25.
+//
+
+import SwiftUI
+
+struct CardDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDetail()
+}
